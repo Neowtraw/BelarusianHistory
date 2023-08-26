d45a822f32f17a402dbe73c81e6a6c4ee8f07156
Neow <kwussq1@gmail.com>
2023-08-26 15:10:40 +0300

    Initial commit

---
+++ app/src/main/java/com/codingub/belarusianhistory/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codingub.belarusianhistory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
